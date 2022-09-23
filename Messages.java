7d5fce739033fd3f43da87a2474986bcdd560240
Ayush Kumar Choudhary <84630014+Ayushkrc@users.noreply.github.com>
2022-09-23 14:40:14 +0530

    Add files via upload

---
+++ Messages.java
@@ -0,0 +1,33 @@
+package com.training.firebase;
+
+public class Messages {
+
+    //Model class
+
+    String name;
+    String imageUrl;
+
+    public Messages() {
+    }
+
+    public Messages(String name, String imageUrl) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
